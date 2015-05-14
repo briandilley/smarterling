0f159ee8444a919540576d9e5a4e16bce2bb8f9c
Brian Dilley <brian.dilley@gmail.com>
2015-05-14 12:24:38 -0700

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
 
     # general meta
     name='smarterling',
-    version='0.4',
+    version='0.5',
     author='Brian C. Dilley - Flipagram',
     author_email='brian@flipagram.com',
     description='Python based command line tool for performing automated tasks with Smartling (http://www.smartling.com).',
